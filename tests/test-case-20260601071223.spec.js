126cb69f6beeb47b5f61ab07c92489349187bfcc
sirol567 <sirolvarshini97@gmail.com>
2026-06-01 16:51:56 +0530

    Change test to go to login page first
    Updated the test to navigate to the login page before accessing secure content.

--- tests/test-case-20260601071223.spec.js
+++ tests/test-case-20260601071223.spec.js
@@ -2,7 +2,7 @@ const { test, expect } = require('@playwright/test');
 
 test('Verify secure content visibility only for authenticated users', async ({ page }) => {
   // Step 1: Login successfully
-  await page.goto('https://the-internet.herokuapp.com/secure');
+  await page.goto('https://the-internet.herokuapp.com/login');
   await page.fill('#username', 'tomsmith');
   await page.fill('#password', 'SuperSecretPassword!');
   await page.click('button:has-text("Login")');
