3c51a5b4f49ae0f06db81038c8053739572fe396
sirol567 <sirolvarshini97@gmail.com>
2026-07-10 15:20:56 +0530

    Delete tests/verify-successful-login-with-valid-credentials-20260710091451.spec.js

--- tests/verify-successful-login-with-valid-credentials-20260710091451.spec.js
+++
@@ -1,9 +0,0 @@
-const { test, expect } = require('@playwright/test');
-
-test('Verify successful login with valid credentials', async ({ page }) => {
-  await page.goto('https://www.saucedemo.com/');
-  await page.fill('[data-testid="username"]', 'tomsmith');
-  await page.fill('[data-testid="password"]', 'SuperSecretPassword!');
-  await page.click('[data-testid="login-button"]');
-  await expect(page).toHaveURL(/inventory.html/);
-});
