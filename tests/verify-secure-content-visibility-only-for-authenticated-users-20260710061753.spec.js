d0a3be2fba56dd5b49d23cdc10d5118497875b84
sirol567 <sirolvarshini97@gmail.com>
2026-07-10 11:59:27 +0530

    Delete tests directory

--- tests/verify-secure-content-visibility-only-for-authenticated-users-20260710061753.spec.js
+++
@@ -1,9 +0,0 @@
-const { test, expect } = require('@playwright/test');
-
-test('Verify secure content visibility only for authenticated users', async ({ page }) => {
-  await page.goto('https://the-internet.herokuapp.com/secure');
-  await page.fill('#username', 'tomsmith');
-  await page.fill('#password', 'SuperSecretPassword!');
-  await page.click('button:has-text("Login")');
-  await expect(page.locator('div#flash')).toContainText('You logged into a secure area!');
-});
