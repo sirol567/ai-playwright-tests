ace11276ca978e4cb9e504e60c7bb22fba00f3fa
sirol567 <sirolvarshini97@gmail.com>
2026-07-20 14:13:56 +0530

    verify-login-with-valid-credentials-20260720084351.spec.js

---
+++ tests/verify-login-with-valid-credentials-20260720084351.spec.js
@@ -0,0 +1,9 @@
+const { test, expect } = require('@playwright/test');
+
+test('Verify login with valid credentials', async ({ page }) => {
+  await page.goto('https://the-internet.herokuapp.com/login');
+  await page.fill('#username', 'tomsmith');
+  await page.fill('#password', 'SuperSecretPassword!');
+  await page.click('button:has-text("Login")');
+  await expect(page.locator('#flash')).toContainText('You logged into a secure area!');
+});
